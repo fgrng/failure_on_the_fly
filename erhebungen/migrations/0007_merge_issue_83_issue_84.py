841fad840edacda5afc6515126dbfff5fa404c42
Fabian Grünig <gruenig@posteo.de>
2026-07-16 07:03:17 +0000

    Merge Sandcastle issues 83 and 84

---
+++ erhebungen/migrations/0007_merge_issue_83_issue_84.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("erhebungen", "0006_erhebungsbindung_randomisierungs_seed_and_more"),
+        ("erhebungen", "0006_randomisierung_normalisiert_positionen"),
+    ]
+
+    operations = []
